7026d9f1049c8648a841d1c71e40f971d34258db
Eduardo Lima <ce-lima@hotmail.com>
2023-06-14 10:56:02 -0300

    PCA v2

--- PCA.py
+++ PCA.py
@@ -2,36 +2,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
+from sklearn.decomposition import PCA
+from collections import namedtuple
 
+def PCA(arr):
+    #Orientação do vetor deve ser:
+    #variáveis (features) nas linhas
+    #indivíduos (samples) nas colunas
+    #Orientação contrária ao exigido pelo Scikit Learn
+    results_PCA = namedtuple("PCA", ["components",
+                                    "explained_variance",
+                                    "explained_variance_ratio",
+                                    "x_transf"])
+    x_arr = arr - np.repeat(arr.mean(axis = 1).reshape(-1,1),
+                        repeats = i_data.shape[1], axis = 1)
+    cov_x = np.cov(x_arr)
+
+    w, v = np.linalg.eig(cov_x)
+    sorted_idx = w.argsort()[::-1]
+    w = w[sorted_idx]
+    v = v[:, sorted_idx]
+
+    z = np.dot(v.T, x_arr)
+
+    ve = np.cumsum(w)/w.sum()
+
+    return (results_PCA(components = v,
+                        explained_variance = w,
+                        explained_variance_ratio = ve,
+                        x_transf = z))
 #%%
 i_data = np.loadtxt("Dados/derm_input.txt")
-# i_data = np.loadtxt("Dados/wine_input.txt")
-#%%
-x_arr = i_data - np.repeat(i_data.mean(axis = 1).reshape(-1,1),
-                        repeats = i_data.shape[1], axis = 1)
 
-cov_x = np.cov(x_arr)
-
-#w autovalor e v autovetor
-#sorted_index = w.argsort()[::-1] 
-#index da ordem crescente dos autovalores, utilizo para ordenar os vetores w e v.
-
-w, v = np.linalg.eig(cov_x)
-sorted_idx = w.argsort()[::-1] #ordem crescente dos autovalores,
-w = w[sorted_idx]
-v_s = v[:,sorted_idx]
-
-Q = v.T
-z = np.dot(Q,x_arr)
-cov_z = np.cov(z)
-
-vt = w.sum()
-ve = np.cumsum(w)/vt
-
+myPCA = PCA(i_data)
 # %%
+sk_PCA = PCA(n_components = 5)
+sk_PCA.fit(i_data.T)
+#%%
 fig, ax = plt.subplots(dpi = 600)
-ax.plot(ve, label = "Cumulative Explained Variance", c = "red")
-ax.bar(x = range(0, len(w)), height = w/vt, label = "Explained Variance")
+ax.plot(myPCA.explained_variance_ratio, label = "Cumulative Explained Variance", c = "red")
+ax.bar(x = range(0, len(myPCA.explained_variance)),
+    height = myPCA.explained_variance/myPCA.explained_variance.sum(),
+    label = "Explained Variance")
 ax.set_ylim(0, 1)
 ax.yaxis.set_major_formatter(PercentFormatter(1))
 ax.set_xlabel("PCA index")
