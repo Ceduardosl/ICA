c77e5b4050f5e93ffe585b2185fda42f77d3f2dd
Eduardo Lima <ce-lima@hotmail.com>
2023-06-16 10:01:58 -0300

    PS v1

--- RNA.py
+++ RNA.py
@@ -1,85 +1,77 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-def fun_output(u):
-    if u < 0:
-        output = 0
-    if u >= 0:
-        output = 1
+def fun_sign(u):
+
+    u[np.where(u >= 0)] = 1
+    u[np.where(u < 0)] = 0
     
-    return output
-
-d = np.array([0,1,1,1])
-bias = 0
-n = 0.5
-w = np.array([bias, 0,0])
-x = np.array([
-    [0, 0, 1, 1],
-    [0, 1, 0, 1]])
-x = np.insert(x, 0, np.full(x.shape[1], -1), axis = 0)
-
+    return u
 #%%
+#data = pxn
+i_data = np.loadtxt("Dados/derm_input.txt")
+o_data = np.loadtxt("Dados/derm_target.txt")
+eta = 0.01
+Ne = 100
+Nr = 10
+#%%
+for r in range(Nr):
+    rand_index = np.random.permutation(i_data.shape[1])
 
-print("Pesoas iniciais - w = {}\n".format(w))
-count = 0
-n_rod = 5
-n_era = 2
+    #Embaralhamento a cada rodada
+    i_data = i_data[:, rand_index]
+    o_data = o_data[:, rand_index]
 
-for i in range(n_rod):
-    print("Rodada {}".format(i))
-    print("Vetor de Entrada {}".format(x[1:,:]))
-    print("Saida Alvo {}\n".format(d))
-    for j in range(n_era):
-        print("Época {}".format(j+1))
-        for k in range(x.shape[1]):
+    spt_point = 0.8 #split point 100*spt_point%
+    t_input = i_data[:,0:int(spt_point*i_data.shape[1])]
+    t_output = o_data[:,0:int(spt_point*i_data.shape[1])]
+    v_input = i_data[:,int(spt_point*i_data.shape[1]):]
+    v_output = o_data[:,int(spt_point*i_data.shape[1]):]
+
+    #Definindo arquitetura
+    #q = número de neuronios
+    #Inicialização Aleatória
+    #Inicialização Aleatória
+    #W = q x (p+1)
+
+    W = np.random.rand(v_output.shape[0], t_input.shape[0]+1)
+
+    #Treino
+    RMSE_plot = []
+    for ep in range(Ne):
+        rand_index = np.random.permutation(t_input.shape[1])
+        t_input = t_input[:,rand_index]
+        t_output = t_output[:,rand_index]
+        RMSE = 0
+        for i in range(t_input.shape[1]):
+            X = np.append(-1, t_input[:,i])
+            U = np.dot(W, X)
+            Y = fun_sign(U)
+            err = t_output[:,i] - Y
+
+            X = np.expand_dims(X, 1)
+            err = np.expand_dims(err, 1)
+            RMSE = RMSE + 0.5*np.power(err, 2).sum()
+            W = W + eta*np.dot(err, X.T)
+        
+        RMSE_plot.append(RMSE)
+        print("Epoca = {}: RMSE = {:.2f}".format(ep, RMSE))
+
+        #Validação
+    count = 0
+    for j in range(v_input.shape[1]):
+        X = np.append(-1, v_input[:,j])
+        U = np.dot(W, X)
+        Y = fun_sign(U)
+        # err = v_output[:,j] - Y
+        if v_output[:,j].argmax() == Y.argmax():
             count += 1
-            u = np.dot(w, x[:,k].T)
-            y = fun_output(u)
 
-            print("iteração {} - w_old = {}".format(count, w))
-            w = w + n*(d[k] - y)*x[:,k].T
+    print(count/v_input.shape[1])
 
-            print("iteração {} - x = {}".format(count, x[:,k]))
-            print("iteração {} - w_new = {}".format(count, w))
-            print("iteração {} - u = {}".format(count, u))
-            print("iteração {} - y = {}".format(count, y))
-            print("iteração {} - erro = {}\n".format(count, d[k]-y))
-    
-    rand_index = np.random.permutation(x.shape[1])
-    x = x[:, rand_index]
-    d = d[rand_index]
 # %%
-g_x1, g_x2 = np.meshgrid(np.linspace(0,1, 100), np.linspace(0,1,100))
-
-x1_arr, x2_arr = (g_x1.flatten(), g_x2.flatten())
-
-X = np.vstack((x1_arr, x2_arr))
-X = np.insert(X, 0, np.full(X.shape[1], -1), axis = 0)
-y_list = []
-for n in range(X.shape[1]):
-    u = np.dot(w, X[:,n].T)
-    y_list.append(fun_output(u))
-
-
-X = np.insert(X, X.shape[0], y_list, axis = 0)
-
-df = pd.DataFrame(X.T, columns = ["bias", "x1", "x2", "y"])
-df.sort_values(by = ["x1", "x2"], ignore_index = True, inplace = True)
-trans_list = []
-for i in range(len(df)-1):
-    if df.y.iloc[i+1] - df.y.iloc[i] == 1:
-        trans_list.append([df.x1.iloc[i], df.x2.iloc[i]])
-df_trans = pd.DataFrame(trans_list, columns = ["x1","x2"])
-#%%
 fig, ax = plt.subplots(dpi = 600)
-ax.scatter(df.x1.loc[df.y == 0], df.x2.loc[df.y == 0], c = "green", label = "Class 1")
-ax.scatter(df.x1.loc[df.y == 1], df.x2.loc[df.y == 1], c = "blue", label = "Class 2")
-ax.plot(df_trans.x1, df_trans.x2, c = "red", label = "Decision Surface", lw = 3)
-ax.scatter(0,0, c = "green", edgecolors = "black", s = 100)
-ax.scatter([0,1,1], [1,0,1], c = "blue", edgecolors = "black", s = 100, zorder = 3)
-ax.set_title("PS - Porta Lógica OR")
-fig.savefig("teste.png", dpi = 600, bbox_inches = "tight", facecolor = "w")
-# %%
-
+ax.plot(RMSE_plot)
+ax.set_ylabel("RMSE")
+ax.set_xlabel("Step")
 # %%
